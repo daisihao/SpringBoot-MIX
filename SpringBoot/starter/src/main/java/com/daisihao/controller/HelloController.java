44e1ab8467bb1b24f044bf49bd08cbb29429ece7
代思豪 <798814330@qq.com>
2019-03-18 19:45:40 +0800

    gitignore
    gitignore

---
+++ SpringBoot/starter/src/main/java/com/daisihao/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.daisihao.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @RequestMapping("/")
+    public String hello() {
+        logger.info("=======输出INFO信息=======");
+        return "hello";
+    }
+}
