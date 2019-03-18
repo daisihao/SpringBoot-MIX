9861a580674e7121fae2811f6ddc3e4f8337b4ba
代思豪 <798814330@qq.com>
2019-03-18 14:49:30 +0800

    ComponentScan
    ComponentScan扫描包

--- SpringBoot/Junit/src/main/java/com.daisihao.controller/HelloController.java
+++ SpringBoot/Junit/src/main/java/com.daisihao.controller/HelloController.java
@@ -7,16 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
 public class HelloController {
-
     @RequestMapping("/")
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "hello";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloController.class,args);
     }
 }
