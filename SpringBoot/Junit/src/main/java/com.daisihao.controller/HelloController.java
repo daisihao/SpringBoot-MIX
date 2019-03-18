cfaad5274a6ddb89e05c5b837207b3349059c52f
代思豪 <798814330@qq.com>
2019-03-18 15:05:04 +0800

    @PathVariable
    接收url中的参数

--- SpringBoot/Junit/src/main/java/com.daisihao.controller/HelloController.java
+++ SpringBoot/Junit/src/main/java/com.daisihao.controller/HelloController.java
@@ -3,12 +3,14 @@ package com.daisihao.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
     @RequestMapping("/")
     public String hello() {
         return "hello";
@@ -17,5 +19,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String helloword() {
         return "helloword";
+    }
+    //接收请求中的参数
+    @RequestMapping("info/{msg}")
+    public String Show(@PathVariable String msg){
+        return msg;
     }
 }
