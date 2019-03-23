03bb9098c2c65429520830da229bf386f5f0563f
代思豪 <798814330@qq.com>
2019-03-23 15:35:58 +0800

    自定义拦截器，注册，拦截所有请求

---
+++ SpringBoot/starter/src/main/java/com/daisihao/interceptor/MyInterceptor.java
@@ -0,0 +1,34 @@
+package com.daisihao.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //定义拦截器
+        HandlerInterceptor interceptor = new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+                System.out.println("自定义拦截器");
+                return true;
+            }
+
+            @Override
+            public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+            }
+        };
+        registry.addInterceptor(interceptor).addPathPatterns("/**");
+    }
+}
