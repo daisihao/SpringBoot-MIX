ca0e0f3531ee0af321afb530007aa9082043233a
代思豪 <798814330@qq.com>
2019-03-29 16:19:35 +0800

    批量上送文件

--- SpringBoot/UploadFiles/src/main/java/com/daisihao/controller/FileController.java
+++ SpringBoot/UploadFiles/src/main/java/com/daisihao/controller/FileController.java
@@ -45,4 +45,31 @@ public class FileController {
         }
         return "上传成功";
     }
+
+    @RequestMapping(value = "/uploads",method = RequestMethod.POST)
+    @ResponseBody
+    public String uploadBatch(MultipartFile[] file, HttpServletRequest httpServletRequest){
+        try {
+            //第一步,创建文件在服务器中的存放路径
+            String path = httpServletRequest.getServletContext().getRealPath("/upload");
+            //第二步,创建文件夹对象
+            File fileDir = new File(path);
+            //第三步,判断文件夹是否存在
+            if(!fileDir.exists()){
+                fileDir.mkdir();
+            }
+            //第四步,获取上传文件名字
+            for(int i=1;i<file.length;i++){
+                String originalFilename = file[i].getOriginalFilename();
+                //第五步,创建文件对象
+                File filedes = new File(fileDir+"/"+originalFilename);
+                //写入文件
+                file[i].transferTo(filedes);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return "上传失败";
+        }
+        return "上传成功";
+    }
 }
